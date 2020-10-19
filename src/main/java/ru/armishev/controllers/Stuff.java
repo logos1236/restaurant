87aef3dec7bad40c5e659891c52bc782bb1648d9
logos <armishev@vigroup.ru>
2020-10-19 22:26:49 +0500

    Error page

--- src/main/java/ru/armishev/controllers/Stuff.java
+++ src/main/java/ru/armishev/controllers/Stuff.java
@@ -5,11 +5,16 @@ import com.google.gson.FieldAttributes;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 import ru.armishev.dao.WaiterDAO;
 import ru.armishev.dao.WaiterSevice;
+import ru.armishev.entity.stuff.Waiter;
+
+import java.util.Optional;
 
 @Controller
 
@@ -30,7 +35,12 @@ public class Stuff {
     @ResponseBody
     public String getById(@PathVariable Integer id) {
         Gson g = new GsonBuilder().addSerializationExclusionStrategy(WaiterDAO.public_strategy).create();
+        Optional<Waiter> result = waiterDAO.getById(id);
 
-        return g.toJson(waiterDAO.getById(id).get());
+        if (result.isPresent()) {
+            return g.toJson(waiterDAO.getById(id).get());
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Unable to find resource");
+        }
     }
 }
